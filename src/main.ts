bc5d68b07e98d7bd0dd8d37a198832e86474bc09
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-07-20 10:45:09 +0100

    feat: Create Mobile ui

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
-import './style.scss'
-import { setupCounter } from './counter'
+import './styles/style.scss'
+import {setImgSource} from './utils'
+import wk1 from './assets/images/wk1.jpg'
+import wk2 from './assets/images/wk2.jpg'
+import wk3 from './assets/images/wk3.jpg'
+import wk4 from './assets/images/wk4.jpg'
+import wk5 from './assets/images/wk5.jpg'
+import wk6 from './assets/images/wk6.jpg'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    Welcome to fitness
-  </div>
-`
+import zod from './assets/images/zod.jpg'
+import girl from './assets/images/girl.jpg'
+import guy from './assets/images/guy.jpg'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const workout_images = [ wk1, wk2, wk3 , wk4 , wk5 , wk6]
+
+const testi__users = [girl, guy, zod]
+
+
+document.querySelectorAll<HTMLImageElement>(".goal__img").forEach((el:HTMLImageElement , index:number)=>{
+    setImgSource(el,workout_images[index])
+})
+
+
+document.querySelectorAll<HTMLImageElement>(".testimonials__userPic").forEach((el:HTMLImageElement, index:number)=>{
+    setImgSource(el,testi__users[index])
+})
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export const setImgSource=(el:HTMLImageElement, url:string)=>{
+    el.src = url
+}
